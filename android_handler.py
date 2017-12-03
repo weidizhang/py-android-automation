878eb29ba6697b036e1f9a48b210ac09018afa67
Weidi Zhang <weidiz999@yahoo.com>
2017-12-02 21:41:38 -0800

    Android Handler: Support tapping coordinate on screen and getting screen as an image

---
+++ android_handler.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from PIL import Image
+
+import os
+
+class AndroidHandler:
+    def tap_screen(self, x: int, y: int) -> None:
+        self._run_shell_command('input tap ' + x + ' ' + y)
+
+    def get_screen(self) -> Image:
+        screenshot_file_name = 'scrn.png'
+        screenshot_file = Path(screenshot_file_name)
+
+        if screenshot_file.exists():
+            screenshot_file.unlink()
+
+        self._run_shell_command('screencap -p /sdcard/' + screenshot_file_name)
+        self._run_adb_command('pull /sdcard/'+ screenshot_file_name)
+        self._run_shell_command('rm /sdcard/' + screenshot_file_name)
+
+        return Image.open(screenshot_file_name)
+
+    def _run_shell_command(self, command: str) -> None:
+        self._run_adb_command('shell ' + command)
+
+    def _run_adb_command(self, command: str) -> None:
+        os.system('adb ' + command)

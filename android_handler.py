5596414de55e0c69e7b7c5557e7ca4896a24a9ce
Weidi Zhang <weidiz999@yahoo.com>
2017-12-03 19:54:27 -0800

    Android Handler: Make command methods follow public style, start/stop app

--- android_handler.py
+++ android_handler.py
@@ -5,7 +5,7 @@ import subprocess
 
 class AndroidHandler:
     def tap_screen(self, x: int, y: int) -> None:
-        self._run_shell_command('input tap ' + str(x) + ' ' + str(y))
+        self.run_shell_command('input tap ' + str(x) + ' ' + str(y))
 
     def get_screen(self) -> Image:
         screenshot_file_name = 'scrn.png'
@@ -14,15 +14,21 @@ class AndroidHandler:
         if screenshot_file.exists():
             screenshot_file.unlink()
 
-        self._run_shell_command('screencap -p /sdcard/' + screenshot_file_name)
-        self._run_adb_command('pull /sdcard/'+ screenshot_file_name)
-        self._run_shell_command('rm /sdcard/' + screenshot_file_name)
+        self.run_shell_command('screencap -p /sdcard/' + screenshot_file_name)
+        self.run_adb_command('pull /sdcard/'+ screenshot_file_name)
+        self.run_shell_command('rm /sdcard/' + screenshot_file_name)
 
         return Image.open(screenshot_file_name)
 
-    def _run_shell_command(self, command: str) -> None:
-        self._run_adb_command('shell ' + command)
+    def launch_app(self, package_name: str) -> None:
+        self.run_shell_command('monkey -p ' + package_name + ' 1')
 
-    def _run_adb_command(self, command: str) -> None:
+    def stop_app(self, package_name: str) -> None:
+        self.run_shell_command('am force-stop ' + package_name)
+
+    def run_shell_command(self, command: str) -> None:
+        self.run_adb_command('shell ' + command)
+
+    def run_adb_command(self, command: str) -> None:
         adb_process = subprocess.Popen('adb ' + command)
         adb_process.wait()

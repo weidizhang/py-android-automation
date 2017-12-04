2ba0610be44eb6d6052f4fd25409e1d72f4eb7bf
Weidi Zhang <weidiz999@yahoo.com>
2017-12-03 20:43:57 -0800

    BotBase: Add default sleep time, add method to change loop sleep time

--- bot_base.py
+++ bot_base.py
@@ -5,13 +5,13 @@ import abc
 import time
 
 class BotBase(abc.ABC):
-    def __init__(self, loop_sleep_time: int):
+    def __init__(self, loop_sleep_time: float = 0.1):
         'loop_sleep_time is given in seconds'
 
         self._android_handler = android_handler.AndroidHandler()
         self._pixel_check = pixel_check.PixelCheck()
 
-        self._loop_sleep = loop_sleep_time
+        self.set_loop_sleep(loop_sleep_time)
 
     def run(self) -> None:
         while True:
@@ -20,6 +20,9 @@ class BotBase(abc.ABC):
 
     def wait(self, seconds: float) -> None:
         time.sleep(seconds)
+
+    def set_loop_sleep(self, seconds: float) -> None:
+        self._loop_sleep = seconds
 
     @abc.abstractmethod
     def do_events(self) -> None:
